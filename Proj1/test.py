522977aa59c924dd6695bf30a85fcd9adc2859ee
Baptiste Hériard-Dubreuil <baptiste.heriard@gmail.com>
2019-03-27 22:22:33 +0100

    Modify structure to class

--- Proj1/test.py
+++ Proj1/test.py
@@ -22,42 +22,39 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(128, 256)
         self.fc2 = nn.Linear(256, 2)
 
+        self.mini_batch_size = 100
+        self.eta = 1e-3
+        self.criterion = nn.CrossEntropyLoss()
+
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=5, stride=5))
         x = F.relu(self.fc1(x.view(-1, 128)))
         x = self.fc2(x)
         return x
 
+    # Training Function
+    def train(self, train_input, train_target):
+        for e in range(100):
+            sum_loss = 0
+            for b in range(0, train_input.size(0), self.mini_batch_size):
+                output = self(train_input.narrow(0, b, self.mini_batch_size))
+                loss = self.criterion(output, train_target.narrow(0, b, self.mini_batch_size))
+                self.zero_grad()
+                loss.backward()
+                sum_loss = sum_loss + loss.item()
+                for p in self.parameters():
+                    p.data.sub_(self.eta * p.grad.data)
+            print("Step %d : %f" % (e, sum_loss))
 
-# Training Function
-
-def train_model(model, train_input, train_target):
-    criterion = nn.CrossEntropyLoss()
-    eta = 1e-3
-
-    for e in range(100):
-        sum_loss = 0
-        for b in range(0, train_input.size(0), mini_batch_size):
-            output = model(train_input.narrow(0, b, mini_batch_size))
-            loss = criterion(output, train_target.narrow(0, b, mini_batch_size))
-            model.zero_grad()
-            loss.backward()
-            sum_loss = sum_loss + loss.item()
-            for p in model.parameters():
-                p.data.sub_(eta * p.grad.data)
-        print("Step %d : %f" % (e, sum_loss))
-
-
-# Test error
-
-def compute_nb_errors(model, input_data, target, mini_batch_size):
-    nb_errors = 0
-    for b in range(0, input_data.size(0), mini_batch_size):
-        output = model(input_data.narrow(0, b, mini_batch_size))
-        predictions = output.argmax(1)
-        target_labels = target.narrow(0, b, mini_batch_size)
-        nb_errors += torch.sum(target_labels != predictions)
-    return float(nb_errors) * 100 / input_data.size(0)
+    # Test error
+    def nb_errors(self, input_data, target):
+        nb_errors = 0
+        for b in range(0, input_data.size(0), self.mini_batch_size):
+            output = self(input_data.narrow(0, b, self.mini_batch_size))
+            predictions = output.argmax(1)
+            target_labels = target.narrow(0, b, self.mini_batch_size)
+            nb_errors += torch.sum(target_labels != predictions)
+        return float(nb_errors) * 100 / input_data.size(0)
 
 
 N_PAIRS = 1000
@@ -80,7 +77,7 @@ mu, std = train_input.mean(0), train_input.std(0)
 train_input.sub_(mu).div_(std)
 test_input.sub_(mu).div_(std)
 """
-train_model(my_model, train_input, train_target)
+my_model.train(train_input, train_target)
 print("Train error : %.1f%% \nTest error : %.1f%%" %
-      (compute_nb_errors(my_model, train_input, train_target, mini_batch_size),
-       compute_nb_errors(my_model, test_input, test_target, mini_batch_size)))
+      (my_model.nb_errors(train_input, train_target),
+       my_model.nb_errors(test_input, test_target)))
