0bd5c7dd2717dd773795f1ffe36c39a61bae6784
lucien <lucien.barret@gmail.com>
2019-03-29 20:02:44 +0100

    model is working

--- Proj1/model.py
+++ Proj1/model.py
@@ -20,28 +20,8 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x.view(-1, 256)))
         x = self.fc2(x)
         return x
-
-class Net_number(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
-        self.fc1 = nn.Linear(256, 256)
-        self.fc2 = nn.Linear(256, 10)
-
-        self.mini_batch_size = 100
-        self.criterion = nn.CrossEntropyLoss()
-        self.nb_epoch = 25
-        self.optimizer = torch.optim.SGD(self.parameters(), lr=1e-3, momentum=0.9)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=3, stride=3))
-        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2, stride=2))
-        x = F.relu(self.fc1(x.view(-1, 256)))
-        x = self.fc2(x)
-        return x
-
-    # Training Function
+    
+        # Training Function
     def trainer(self, train_input, train_target):
         """
         Train the model on a training set
@@ -58,22 +38,8 @@ class Net_number(nn.Module):
                 sum_loss = sum_loss + loss.item()
                 self.optimizer.step()
             print("Step %d : %f" % (e, sum_loss))
-    
-    def trainer_number(self, train_input, train_target):
-        """
-        """
-        for e in range(self.nb_epoch):
-            sum_loss = 0
-            for b in range(0, train_input.size(0), self.mini_batch_size):
-                output = self(train_input.narrow(0, b, self.mini_batch_size))
-                loss = self.criterion(output, train_target.narrow(0, b, self.mini_batch_size))
-                self.optimizer.zero_grad()
-                loss.backward()
-                sum_loss = sum_loss + loss.item()
-                self.optimizer.step()
-            print("Step %d : %f" % (e, sum_loss))
-
-    # Test error
+            
+        # Test error
     def nb_errors(self, input_data, target):
         """
         Compute the number of error of the model on a test set
@@ -87,4 +53,75 @@ class Net_number(nn.Module):
             predictions = output.argmax(1)
             target_labels = target.narrow(0, b, self.mini_batch_size)
             nb_errors += torch.sum(predictions != target_labels)
+        return float(nb_errors) * 100 / input_data.size(0)
+
+class Net_number(nn.Module):
+    def __init__(self):
+        super(Net_number, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(256, 256)
+        self.fc2 = nn.Linear(256, 10)
+        self.mini_batch_size = 100
+        self.criterion = nn.CrossEntropyLoss()
+        self.nb_epoch = 25
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=1e-3, momentum=0.9)
+        
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=2, stride=2))
+        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2, stride=2))
+        x = F.relu(self.fc1(x.view(-1, 256)))
+        x = self.fc2(x)
+        return x
+    
+    def trainer_number(self, train_input, train_classes):
+        """
+        """
+        train_input=train_input.view(2000,1,14,14)
+        train_target=torch.empty(2000,10)
+        for i in range(2000):
+            for j in range(10):
+                if j == train_classes.view(2000,1)[i,0]:
+                    train_target[i,j]=1
+                else:
+                    train_target[i,j]=0
+        #c'est moche mais je crois que ca marche. à changer.
+        print(train_target)
+#        for e in range(self.nb_epoch):
+#            sum_loss = 0
+#            for b in range(0, train_input.size(0), self.mini_batch_size):
+#                output = self(train_input.narrow(0, b, self.mini_batch_size))
+#                loss = self.criterion(output, train_target.narrow(0, b, self.mini_batch_size))
+#                self.optimizer.zero_grad()
+#                loss.backward()
+#                sum_loss = sum_loss + loss.item()
+#                self.optimizer.step()
+#            print("Step %d : %f" % (e, sum_loss))
+
+    # Test error
+    def nb_errors_number(self, input_data, target):
+        """
+        Compute the number of error of the model on a test set
+        :param input_data: test features
+        :param target: test target
+        :return: number of errors
+        """
+        nb_errors = 0
+        input_data=input_data.view(2000,1,14,14)
+        print(input_data.size())
+        for b in range(0, input_data.size(0), self.mini_batch_size):
+            output = self(input_data.narrow(0, b, self.mini_batch_size))
+            _, predicted_classes = output.data.max(1)
+            print(predicted_classes)
+            print(predicted_classes.size())
+#            for k in range(self.mini_batch_size):
+#                nb_errors += torch.sum()
+#                    
+#                    
+#        nb_errors = 0
+#        for b in range(0, input_data.size(0), self.mini_batch_size):
+#            output = self(input_data.narrow(0, b, self.mini_batch_size))
+#            predictions = 0
+#            target_labels = target.narrow(0, b, self.mini_batch_size)
+#            nb_errors += torch.sum(predictions != target_labels)
         return float(nb_errors) * 100 / input_data.size(0)
--- Proj1/test.py
+++ Proj1/test.py
@@ -9,6 +9,7 @@ Created on Wed Mar  6 10:36:43 2019
 from torch.autograd import Variable
 import dlc_practical_prologue as prologue
 from model import Net
+from model import Net_number
 
 
 """ Load Data """
@@ -21,16 +22,16 @@ train_input, train_target = Variable(train_input), Variable(train_target)
 test_input, test_target = Variable(test_input), Variable(test_target)
 
 """ Create and train model """
-my_model = Net()
-
-my_model.trainer(train_input, train_target)
-print("Train error : %.1f%% \nTest error : %.1f%%" %
-      (my_model.nb_errors(train_input, train_target),
-       my_model.nb_errors(test_input, test_target)))
+#my_model = Net()
+#
+#my_model.trainer(train_input, train_target)
+#print("Train error : %.1f%% \nTest error : %.1f%%" %
+#      (my_model.nb_errors(train_input, train_target),
+#       my_model.nb_errors(test_input, test_target)))
 
 """ Create and train model which identifies each number and then compares them """
-my_model = Net_number()
-my_model.trainer_number(train_input, train_target)
+my_model_number = Net_number()
+my_model_number.trainer_number(train_input, train_classes)
 print("Train error : %.1f%% \nTest error : %.1f%%" %
-      (my_model.nb_errors(train_input, train_target),
-       my_model.nb_errors(test_input, test_target)))
+      (my_model_number.nb_errors_number(train_input, train_target),
+       my_model_number.nb_errors_number(test_input, test_target)))
