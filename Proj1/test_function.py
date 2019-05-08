f1f3378a9ac8f1e2981ecc7694154a57580009bf
Unknown <lucien.barret@gmail.com>
2019-05-08 15:32:36 +0200

    lol nop
    le notebook appelle la fonction pour tester plusieurs fois le model avec différentes seeds, et on calcule le stdev et la moyenne.

---
+++ Proj1/test_function.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 10:36:43 2019
+
+@author: Darcane
+"""
+
+from torch.autograd import Variable
+import dlc_practical_prologue as prologue
+from my_nets import *
+
+
+""" Load Data """
+def test():
+    N_PAIRS = 1000
+
+    train_input, train_target, train_classes, test_input, test_target, test_classes = \
+        prologue.generate_pair_sets(N_PAIRS)
+
+    train_input, train_target = Variable(train_input), Variable(train_target)
+    test_input, test_target = Variable(test_input), Variable(test_target)
+
+    """ Create and train model """
+    my_model = NetWithWeightSharing()
+
+
+    """ Create and train model which identifies each number and then compares them """
+    my_model.trainer(train_input, train_target)
+
+    print("Train error : %.1f%% \nTest error : %.1f%%" %
+          (my_model.nb_errors(train_input, train_target),
+           my_model.nb_errors(test_input, test_target)))
+
+    print("Train error : %.1f%% \nTest error : %.1f%%" %
+          (my_model.nb_errors(train_input, train_target),
+           my_model.nb_errors(test_input, test_target)))
+
+
+    return my_model.nb_errors(test_input, test_target)
